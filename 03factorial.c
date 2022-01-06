fae5c0f1773c68f53dc7b47d660b744e24cc96b0
shivani <93367797+srishivanic@users.noreply.github.com>
2022-01-06 14:16:43 +0530

    Create 03factorial.c

---
+++ 03factorial.c
@@ -0,0 +1,13 @@
+/Develop a C program to find factorial of a number./
+  
+  /Algorithm/
+  1.Start 
+  2.Enter an integer
+  3.Read the intger and assign it to a variable
+  4.From the value of the integer up to 1 multiply each digit and update final value
+  5.The final value at the end of all the multiplication till 1 is the factorial
+  6.Stop
+  
+  
+  /Program/
+  
